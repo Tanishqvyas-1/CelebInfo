34bccf0f9c22b6c8f8c0039ed1f8934c298fc9f1
Tanishq vyas <91602855+Tanishqvyas-1@users.noreply.github.com>
2023-11-16 09:55:59 +0530

    Add files via upload

---
+++ celebinfo.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var introSection = document.querySelector('.intro');
+
+    introSection.classList.add('animate');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var introSection = document.querySelector('.intro2');
+
+    introSection.classList.add('animate');
+});
